0a8ff26bec348426ad71baa2e3640a2bd9248813
aLiruihao <1262946752@qq.com>
2019-11-12 14:52:38 +0800

    Initialization

---
+++ src/main/java/com/lrh/App.java
@@ -0,0 +1,13 @@
+package com.lrh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
