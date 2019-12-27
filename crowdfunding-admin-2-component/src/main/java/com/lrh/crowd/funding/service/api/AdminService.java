be19dd180b88b853ab9eac8179bbe9ad6049e993
aLiruihao <1262946752@qq.com>
2019-12-27 14:55:53 +0800

    mybatis逆向工程生成用户登录表相关文件

---
+++ crowdfunding-admin-2-component/src/main/java/com/lrh/crowd/funding/service/api/AdminService.java
@@ -0,0 +1,9 @@
+package com.lrh.crowd.funding.service.api;
+
+/**
+ * @author lrhya
+ * @version 1.0
+ * @date 2019/12/27 13:56
+ */
+public interface AdminService {
+}
---
+++ crowdfunding-admin-2-component/src/main/java/com/lrh/crowd/funding/service/impl/AdminServiceImpl.java
@@ -0,0 +1,14 @@
+package com.lrh.crowd.funding.service.impl;
+
+import com.lrh.crowd.funding.service.api.AdminService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lrhya
+ * @version 1.0
+ * @date 2019/12/27 13:57
+ */
+
+@Service
+public class AdminServiceImpl  implements AdminService {
+}
